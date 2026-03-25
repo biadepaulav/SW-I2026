964602c26b5edfdace695f279c16ce23574047db
biadepaulav <beatriz.de.paula290@gmail.com>
2026-03-25 08:49:08 -0300

    Add files via upload

---
+++ aula2503/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <script src="script.js"></script>
+    <title>Média de notas - Aula 25/03</title>
+</head>
+<body>
+    <main>
+        <div class = "formulario">
+            <div>
+                <h4>Digite o seu nome:</h4>
+                <input type="text" id="nome">
+            </div>
+
+            <div>
+            <h4>Digite o seu sobrenome:</h4>
+                <input type="text" id="sobrenome">
+            </div>
+
+            <div>
+                <h4>Digite a sua primeira nota:</h4>
+                <input type="text" id="nota1">
+            </div>
+
+            <div>
+                <h4>Digite a sua segunda nota:</h4>
+                <input type="text" id="nota2">
+            </div>
+
+            <div>
+                <h4>Digite a sua terceira nota:</h4>
+                <input type="text" id="nota3">
+            </div>
+           
+            <br>
+            <br>
+
+            <div class = "botao">
+                <button type="submit">Enviar</button>
+            </div>
+        </div>
+        
+
+        <?php 
+
+        ?>
+    </main>
+    
+</body>
+</html>
---
+++ aula2503/script.js
@@ -0,0 +1,8 @@
+var $nome;
+var $sobrenome;
+var $nota1;
+var $nota2;
+var $nota3;
+
+let
+alert 
