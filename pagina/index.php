f2a5b17c634c87afa76ae989fed8c66c2ab3335c
biadepaulav <beatriz.de.paula290@gmail.com>
2026-04-22 08:49:25 -0300

    Add files via upload

---
+++ pagina/index.php
@@ -0,0 +1,27 @@
+<?php
+$_MENU = [
+        'Home' => '<h1>Pagina inicial</h1>',
+        'Sobre' => '<h1>Sobre page aqui</h1>',
+        'Contato' => '<h1>Contato page aqui</h1>',
+        'Experiencias' => '<h1>Experiencias page aqui</h1>',
+        'Projetos' => '<h1>Projetos page aqui</h1>',
+        ]
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>PHP test</title>
+</head>
+<header>
+    
+</header>
+<body>
+    
+</body>
+</html>
